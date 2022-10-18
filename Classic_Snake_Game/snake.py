5990440fe5a6f3ad9552f0fce061a87ad9a57055
AnkuRana <amitrana.com0007@gmail.com>
2022-10-18 16:37:01 +0530

    Code refactor

--- Classic_Snake_Game/snake.py
+++ Classic_Snake_Game/snake.py
@@ -1,12 +1,17 @@
 from turtle import Turtle
 
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
         self.snake_turtle = []
         self.create_snake()
+        self.snake_head = self.snake_turtle[0]
 
     def create_snake(self):
         pos = 0
@@ -23,16 +28,20 @@ class Snake:
             new_x = self.snake_turtle[snake - 1].xcor()
             new_y = self.snake_turtle[snake - 1].ycor()
             self.snake_turtle[snake].goto(new_x, new_y)
-        self.snake_turtle[0].forward(MOVE_DISTANCE)
+        self.snake_head.forward(MOVE_DISTANCE)
 
     def left(self):
-        self.snake_turtle[0].setheading(180)
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
 
     def right(self):
-        self.snake_turtle[0].setheading(0)
+        if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
 
     def up(self):
-        self.snake_turtle[0].setheading(90)
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
 
     def down(self):
-        self.snake_turtle[0].setheading(270)
+        if self.snake_head.heading() != UP:
+            self.snake_turtle[0].setheading(DOWN)
