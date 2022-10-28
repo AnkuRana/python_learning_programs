5213224e2e1ca1bbac88c8781ec80f2ae58881a1
AnkuRana <amitrana.com0007@gmail.com>
2022-10-28 22:47:10 +0530

    Exception Handling

--- ListComprehension/main.py
+++ ListComprehension/main.py
@@ -6,8 +6,13 @@ nato_dic = {row.letter: row.code for (index, row) in data.iterrows()}
 to_continue = True
 while to_continue:
     name = input("Enter the name: ").upper()
-    result = [nato_dic[character] for character in name]
-    print(result)
-    option = input("Do you want to continue y|n: ").lower()
-    if option != "y":
-        to_continue = False
+    try:
+        result = [nato_dic[character] for character in name]
+    except KeyError:
+        print("Only letters in the alphabet are allowed!")
+    else:
+        print(result)
+    finally:
+        option = input("Do you want to continue y|n: ").lower()
+        if option != "y":
+            to_continue = False
